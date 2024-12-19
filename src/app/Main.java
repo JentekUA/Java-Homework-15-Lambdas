9589dae9a7665a3a8554c8e3a540ebc9723202dd
oleksandr.aleksashyn <oleksandr.aleksashyn@gmail.com>
2024-12-19 12:40:04 +0200

    Add MathOperation interface

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,14 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        MathOperation operation = new MathOperation() {
+            @Override
+            public int operate(int a, int b) {
+                return a + b;
+            }
+        };
+        System.out.println(operation.operate(40, 2));
+
+
     }
 }
